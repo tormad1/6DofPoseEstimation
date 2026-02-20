92f7b1dd49a20b138ce1cc390c2868a00fd9540e
tormad1 <tormad1000000@gmail.com>
2026-02-20 07:29:42 +0000

    added dummy position and rotation changes

--- DummyPose/DummyPose.cpp
+++ DummyPose/DummyPose.cpp
@@ -39,6 +39,22 @@ extern "C" int __cdecl GetDummyPose(DummyPose* out_pose)
         const std::int64_t now_us = NowMicrosMonotonic();
         const double t = (now_us - g_t0_us) * 1e-6; //since start in seconds
 
+        //dummy change for postion 
+        const float r = 0.5f; //radius of unity object in unity units
+        out_pose->px = 0.5f * static_cast<float>(std::sin(t));
+        out_pose->py = 0.1f * static_cast<float>(std::sin(t * 0.2)); //t * 0.2 changes the speed while the 0.2f * sets the amplitude
+        out_pose->pz = 0.3f * static_cast<float>(std::sin(t * 1.3));
+
+        //dummy change for rotation
+        const float half = static_cast<float>(t * 0.4);
+        out_pose->qx = 0.0f;
+        out_pose->qy = std::sin(half);
+        out_pose->qz = 0.0f;
+        out_pose->qw = std::cos(half);
+
+        //base dummy confidence
+        out_pose->confidence = 0.9f;
+
         //timestamp increasing
         std::int64_t ts = now_us;
         if (ts <= g_last_ts_us) {
