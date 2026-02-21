62eabc04a0253b2947a4a11e646d2949ece5fe2e
tormad1 <tormad1000000@gmail.com>
2026-02-21 09:39:22 +0000

    added modes for setting invalid states and added rotation spiked frames

--- DummyPose/DummyPose.cpp
+++ DummyPose/DummyPose.cpp
@@ -1,6 +1,7 @@
 #include "DummyPose.h"
 
 #include <cmath>
+#include <limits>
 
 
 extern "C" int __cdecl GetDummyPose(DummyPose* out_pose, std::int64_t timestamp_us, int mode)
@@ -20,9 +21,6 @@ extern "C" int __cdecl GetDummyPose(DummyPose* out_pose, std::int64_t timestamp_
         if (mode == 1) {
             out_pose->confidence = 0.1f;
         }
-        if (mode == 2) {
-            return 0;
-        }
         //dummy change for postion 
         out_pose->px = 0.5f * static_cast<float>(std::sin(t));
         out_pose->py = 0.1f * static_cast<float>(std::sin(t * 0.2)); //t * 0.2 changes the speed while the 0.2f * sets the amplitude
@@ -35,12 +33,36 @@ extern "C" int __cdecl GetDummyPose(DummyPose* out_pose, std::int64_t timestamp_
         out_pose->qz = 0.0f;
         out_pose->qw = std::cos(half);
 
-        //mode 4, postion spiked frame
-        if (mode == 4) {
+        //mode 2, roatation spiked frame
+        if (mode == 2) {
+            const float jumped_half = half + 1.7f; //large step
+            out_pose->qy = std::sin(jumped_half);
+            out_pose->qw = std::cos(jumped_half);
+        }
+
+        //mode 3, position spiked frame
+        if (mode == 3) {
             out_pose->px += 10.0f;
             out_pose->pz += 10.0f;
         }
 
+        //mode 4, invalid rotation
+        if (mode == 4) {
+            out_pose->qx = 0.0f;
+            out_pose->qy = 0.0f;
+            out_pose->qz = 0.0f;
+            out_pose->qw = 0.0f;
+        }
+
+        //mode 5, invalid numeric (NaN)
+        if (mode == 5) {
+            out_pose->px = std::numeric_limits<float>::quiet_NaN();
+        }
+
+        //mode 6, invalid numeric (Inf)
+        if (mode == 6) {
+            out_pose->px = std::numeric_limits<float>::infinity();
+        }
         out_pose->timestamp_us = timestamp_us;
         return 1;
     }
